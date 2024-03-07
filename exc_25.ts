a864b234d9fade0d2ee04ff14c77a82276d3db65
snowworldgithub <160354474+snowworldgithub@users.noreply.github.com>
2024-03-07 23:21:08 +0500

    Add files via upload

---
+++ exc_25.ts
@@ -0,0 +1,16 @@
+// Assign a random color to the alien
+let alien_color: string = ["green", "yellow", "red"][Math.floor(Math.random() * 3)];
+
+console.log("The alien's color is:", alien_color);
+if(alien_color=="green"){
+console.log(" waooo you just earned 5 points")
+
+}
+alien_color = "green"; 
+if (alien_color === "green") 
+{
+  console.log("congrates you have earned 5 points again");
+}
+  else {
+    console.log("");
+}
