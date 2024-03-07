40be004b519a0bd6fbcd20a0b6eb3fac9fe98580
snowworldgithub <160354474+snowworldgithub@users.noreply.github.com>
2024-03-07 22:34:49 +0500

    Add files via upload

---
+++ exc_18.ts
@@ -0,0 +1,14 @@
+let favorite_places: string[] = ["dubai", "saudi arabia", "silicon valley", "Islamabad", "muree"];
+console.log("Original order:", favorite_places);
+console.log("Alphabetical order:", [...favorite_places].sort());
+console.log("Original order (again):", favorite_places);
+console.log("Reverse alphabetical order:", [...favorite_places].sort().reverse());
+console.log("Original order (yet again):", favorite_places);
+favorite_places.reverse();
+console.log("Reversed order:", favorite_places);
+favorite_places.reverse();
+console.log("Back to original order:", favorite_places);
+favorite_places.sort();
+console.log("Sorted in alphabetical order:", favorite_places);
+favorite_places.sort((a, b) => b.localeCompare(a));
+console.log("Sorted in reverse alphabetical order:", favorite_places);
